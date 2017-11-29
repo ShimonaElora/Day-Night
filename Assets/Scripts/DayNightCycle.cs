d65fc4c3376d366a47e4eaeb9f1600187875d9e9
Shimona Elora <umysharma@gmail.com>
2017-11-29 21:22:57 +0530

    Sun path cycle

---
+++ Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour {
+
+    public float initTime;
+    public float[] colors;
+    public float speed;
+    public GameObject sun;
+    public float transYMinSun = -2.3f;
+    public float transYMaxSun = 6f;
+    public float transYToggleSun = 0.38f;
+    public GameObject moon;
+    public float transYMinMoon = -2.3f;
+    public float transYMaxMoon = 6f;
+    public float transYToggleMoon = 0.38f;
+
+    private float timeOfDay;
+    private float day;
+    private bool hasSunRisen;
+    private bool hasSunSet;
+    private bool hasMoonRisen;
+    private bool hasMoonSet;
+    private float fracSun;
+    private float yDiffSun;
+    private float ySun;
+    private float fracMoon;
+    private float yDiffMoon;
+    private float yMoon;
+
+    // Use this for initialization
+    void Start () {
+        timeOfDay = initTime * 60 * 60;
+        yDiffSun = transYMaxSun - transYMinSun;
+        if (timeOfDay > 21600 && timeOfDay <= 43200)
+        {
+            fracSun = (timeOfDay - 21600) / 21600;
+            ySun = Mathf.Lerp(transYMinSun, transYMaxSun, fracSun);
+            if (ySun >= transYToggleSun)
+            {
+                hasSunRisen = true;
+                sun.GetComponentInChildren<Light>().intensity = 1;
+            }
+            else
+            {
+                hasSunRisen = false;
+                sun.GetComponentInChildren<Light>().intensity = 0;
+            }
+            hasSunSet = false;
+            sun.GetComponent<Transform>().position = new Vector3(sun.GetComponent<Transform>().position.x, ySun, sun.GetComponent<Transform>().position.z);
+        } else if (timeOfDay > 43200 && timeOfDay <= 64800)
+        {
+            fracSun = (timeOfDay - 43200) / 21600;
+            ySun = Mathf.Lerp(transYMaxSun, transYMinSun, fracSun);
+            hasSunRisen = true;
+            if (ySun >= transYToggleSun)
+            {
+                hasSunSet = false;
+                sun.GetComponentInChildren<Light>().intensity = 1;
+            }
+            else
+            {
+                hasSunSet = true;
+                sun.GetComponentInChildren<Light>().intensity = 0;
+            }
+            sun.GetComponent<Transform>().position = new Vector3(sun.GetComponent<Transform>().position.x, ySun, sun.GetComponent<Transform>().position.z);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeOfDay = timeOfDay + Time.deltaTime * speed * 100;
+        if (timeOfDay > 21600 && timeOfDay <= 43200)
+        {
+            fracSun = (timeOfDay - 21600) / 21600;
+            Debug.Log(fracSun);
+            ySun = Mathf.Lerp(transYMinSun, transYMaxSun, fracSun);
+            if (ySun >= transYToggleSun)
+            {
+                hasSunRisen = true;
+                sun.GetComponentInChildren<Light>().intensity = 1;
+            }
+            else
+            {
+                hasSunRisen = false;
+                sun.GetComponentInChildren<Light>().intensity = 0;
+            }
+            hasSunSet = false;
+            sun.GetComponent<Transform>().position = new Vector3(sun.GetComponent<Transform>().position.x, ySun, sun.GetComponent<Transform>().position.z);
+        }
+        else if (timeOfDay > 43200 && timeOfDay <= 64800)
+        {
+            fracSun = (timeOfDay - 43200) / 21600;
+            ySun = Mathf.Lerp(transYMaxSun, transYMinSun, fracSun);
+            hasSunRisen = true;
+            if (ySun >= transYToggleSun)
+            {
+                hasSunSet = false;
+                sun.GetComponentInChildren<Light>().intensity = 1;
+            }
+            else
+            {
+                hasSunSet = true;
+                sun.GetComponentInChildren<Light>().intensity = 0;
+            }
+            sun.GetComponent<Transform>().position = new Vector3(sun.GetComponent<Transform>().position.x, ySun, sun.GetComponent<Transform>().position.z);
+        }
+    }
+}
---
+++ Assets/Shaders/ScreenGradient.shader
@@ -0,0 +1,93 @@
+﻿Shader "Custom/ScreenGradient"
+{
+	Properties
+	{
+		_MainTex("Texture", 2D) = "white" {}
+		_NoiseTex("Noise Texture", 2D) = "white" {}
+		_BottomColor("Bottom Color", Color) = (1,1,1,1)
+		_TopColor("Top Color", Color) = (1,1,1,1)
+		_Offset("Offset", float) = 0
+	}
+
+	SubShader
+	{
+		Tags
+		{
+			"Queue" = "Transparent"
+			"IgnoreProjector" = "True"
+			"RenderType" = "Transparent"
+			"PreviewType" = "Plane"
+			"CanUseSpriteAtlas" = "True"
+		}
+
+		Cull Off
+		Lighting Off
+		Blend One OneMinusSrcAlpha
+
+		Pass
+		{
+		CGPROGRAM
+
+		#pragma vertex vert
+		#pragma fragment frag
+		#include "UnityCG.cginc"
+
+		struct appdata_t
+		{
+			float4 position	: POSITION;
+			float2 uv		: TEXCOORD0;
+		};
+
+		struct v2f
+		{
+			float4 position	: SV_POSITION;
+			float2 uv		: TEXCOORD0;
+			half4 color		: COLOR;
+		};
+
+		sampler2D _MainTex;
+		sampler2D _NoiseTex;
+		float4 _MainTex_ST;
+		half4 _BottomColor;
+		half4 _TopColor;
+		float _Offset;
+
+		v2f vert(appdata_t IN)
+		{
+			v2f OUT;
+			OUT.position = UnityObjectToClipPos(IN.position);
+			OUT.uv = TRANSFORM_TEX(IN.uv, _MainTex);
+
+			float factor = mad(OUT.position.y, -0.5, 0.5);
+			factor *= 1 + _Offset*2;
+			factor -= _Offset;
+			factor = clamp(factor, 0, 1);
+			OUT.color = lerp(_BottomColor, _TopColor, factor);
+
+			return OUT;
+		}
+
+		float3 getNoise(float2 uv)
+		{
+			float3 noise = tex2D(_NoiseTex, uv * 100 + _Time * 50);
+			noise = mad(noise, 2.0, -0.5);
+
+			return noise/255;
+		}
+
+		half4 frag(v2f IN) : SV_Target
+		{
+			half4 texCol = tex2D(_MainTex, IN.uv);
+			
+			half4 c;
+			c.rgb = IN.color.rgb + getNoise(IN.uv);
+			c.rgb *= texCol.a;
+			c.a = texCol.a;
+
+			return c;
+		}
+
+		ENDCG
+		}
+	}
+}
